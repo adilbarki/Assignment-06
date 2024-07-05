17b177e689f3d24aff49dde0d6a1355a0e8c7b75
adilbarki <168727835+adilbarki@users.noreply.github.com>
2024-07-05 00:39:23 -0700

    done

---
+++ script.js
@@ -0,0 +1,49 @@
+// QUESTION-01
+let st = " Hello World ";
+st = st.trim();
+console.log(st);
+
+//QUESTION-02
+let as = "Hello World";
+as = as.substring(5, 10);
+console.log(as)
+
+//QUESTION-03
+let ab = "Hello World";
+let sub= ab.substring(0, 5);
+console.log(sub);
+
+//QUESTION-04
+let ac= "Hello World";
+let aub = ac.split("-");
+console.log(aub);
+
+//QUESTION-05
+let ad = ["Hello", "World"];
+let jion = ad.join(" ");
+console.log(jion);
+
+//QUESTION-06
+let ae = "Hello World";
+let aeb = ae.indexOf("o");
+console.log(aeb);
+
+//QUESTION-07
+let af = "Hello World World";
+let afb = af.lastIndexOf("World");
+console.log(afb);
+
+//QUESTION-08
+let ag = "World";
+agb = ag.startsWith("Wor");
+console.log(agb);
+
+//QUESTION-09
+let ah = "Hello World";
+ahb = ah.endsWith("World");
+console.log(ahb);
+
+//QUESTION-10
+let ai = "Hello";
+let includ = ai.includes("o");
+console.log(includ);
